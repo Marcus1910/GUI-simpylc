c252591ff2af9a80a80263a06e991a4039e9c618
Marcus1910 <54845611+Marcus1910@users.noreply.github.com>
2021-10-11 14:44:29 +0200

    robotarm code van simpylc toegevoegd

---
+++ RobotArm/world.py
@@ -0,0 +1,12 @@
+import os
+import sys as ss
+
+ss.path.append (os.path.abspath ('../..')) # If you want to store your simulations somewhere else, put SimPyLC in your PYTHONPATH environment variable
+
+import simpylc as sp
+import robot as rb
+import control as ct
+import visualisation as vs
+import timing as tm
+
+sp.World (ct.Control, rb.Robot, vs.Visualisation, tm.Timing)
